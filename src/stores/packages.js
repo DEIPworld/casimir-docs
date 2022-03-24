c2475b8a03d2d52c2baeeb9281214c042ea8b102
ShmidtAlex <shmidt.alex@gmail.com>
2022-03-24 13:09:41 +0200

    fix: review considered

--- src/stores/packages.js
+++ src/stores/packages.js
@@ -11,22 +11,21 @@ export const usePackagesData = defineStore('packages', {
     fetchAndSetPackage() {
       this.packages = packages.map((elem) => ({
         moduleName: elem.name,
-        /*eslint-disable*/
         moduleEntities: [
-          { id: 'components',
-            content: components.filter(component => component.package === elem.name).map(el => {
-              return {
+          {
+            id: 'components',
+            content: components.filter((component) => component.package === elem.name).map((el) => (
+              {
                 ...el,
                 name: el.displayName
-              }
-            })
+              }))
           },
-          { id: 'classes', content: jsdocs.filter(doc => doc.package === elem.name && doc.kind === 'class') },
-          // { id: 'methods', content: jsdocs.filter(doc => doc.package === elem.name && doc.kind === 'function') },
-          { id: 'typeDefs', content: jsdocs.filter(doc => doc.package === elem.name && doc.kind === 'typedef') },
-          { id: 'constants', content: jsdocs.filter(doc => doc.package === elem.name && doc.kind === 'constant') },
-        ].filter(el => Object.values(el).some(val => val.length > 0))
-      }))
+          { id: 'classes', content: jsdocs.filter((doc) => doc.package === elem.name && doc.kind === 'class') },
+          { id: 'methods', content: jsdocs.filter((doc) => doc.package === elem.name && doc.kind === 'function') },
+          { id: 'typeDefs', content: jsdocs.filter((doc) => doc.package === elem.name && doc.kind === 'typedef') },
+          { id: 'constants', content: jsdocs.filter((doc) => doc.package === elem.name && doc.kind === 'constant') }
+        ].filter((el) => Object.values(el).some((val) => val.length > 0))
+      }));
     }
   }
 });
