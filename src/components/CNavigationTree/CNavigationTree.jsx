930eb748519d1b566e703feb904b0eb5724faf98
ShmidtAlex <shmidt.alex@gmail.com>
2022-03-24 22:25:25 +0200

    feat: navigation tree created, methods, constants and types each has common links

--- src/components/CNavigationTree/CNavigationTree.jsx
+++ src/components/CNavigationTree/CNavigationTree.jsx
@@ -1,28 +1,110 @@
+import { usePackagesData } from '@/stores/packages';
+
 export const CNavigationTree = {
   name: 'CNavigationTree',
-  props: {
-    packages: {
-      type: Array
-    }
-  },
-  setup(props) {
-    const showSubItem = (subitem) => (
-      subitem.map((el) => <li>{el.name}</li>)
+  setup() {
+    const store = usePackagesData();
+    const {
+      packages,
+      getClassesByPackage,
+      getMethodsByPackage,
+      getConstantsByPackage,
+      getTypesByPackage,
+      getComponentsByPackage
+    } = store;
+    const createRoute = (pack, section, childProps = {}) => ({
+      name: section ? `package.${section}` : pack,
+      params: {
+        package: pack,
+        ...childProps
+      }
+    });
+    const generateClasses = (moduleName) => (
+      <div class="pack-list__entities">
+        <div>{getClassesByPackage(moduleName).length ? 'CLASSES' : ''}</div>
+          {getClassesByPackage(moduleName).map((el) => (
+          <div className="pack-list__entities-item">
+            <router-link to={
+              createRoute(
+                moduleName.slice(6),
+                'class',
+                { [el.kind]: el.name || el.displayName, package: el.package.slice(6) }
+              ) }>
+               {el.name}
+            </router-link>
+          </div>
+          ))
+        }
+      </div>
     );
-    const showListItem = (items) => items.map((item) => (
-      <div class="pack-list__entities__item">
-         <div>{item.id.toUpperCase()}</div>
-         <li class="pack-list__entities-item__content">{showSubItem(item.content)}</li>
+
+    const generateComponents = (moduleName) => (
+      <div class="pack-list__entities">
+        <div>{getComponentsByPackage(moduleName).length ? 'COMPONENTS' : ''}</div>
+          {getComponentsByPackage(moduleName).map((el) => (
+            <div className="pack-list__entities-item">
+              <router-link to={
+                createRoute(
+                  moduleName.slice(6),
+                  'component',
+                  { component: el.displayName, package: el.package.slice(6) }
+                ) }>
+                {el.displayName}
+              </router-link>
+            </div>
+          ))
+        }
+      </div>
+    );
+    const generateMethods = (moduleName) => {
+      if (getMethodsByPackage(moduleName).length) {
+        return (
+          <div className="pack-list__entities">
+            <router-link to={createRoute(moduleName.slice(6), 'functions',)}>
+              METHODS
+            </router-link>
+          </div>
+        );
+      }
+      return false;
+    };
+    const generateConstants = (moduleName) => {
+      if (getConstantsByPackage(moduleName).length) {
+        return (
+        <div className="pack-list__entities">
+          <router-link to={ createRoute(moduleName.slice(6), 'constants') }>
+          CONSTANTS
+          </router-link>
+        </div>
+        );
+      }
+      return false;
+    };
+    const generateTypes = (moduleName) => {
+      if (getTypesByPackage(moduleName).length) {
+        return (
+        <div className="pack-list__entities">
+          <router-link to={ createRoute(moduleName.slice(6), 'typedefs') }>
+            TYPES
+          </router-link>
+        </div>
+        );
+      }
+      return false;
+    };
+
+    const generateList = () => packages.map((pack) => (
+      <div class="pack-list">
+        <div class="pack-list__name">{pack.name}</div>
+          {generateClasses(pack.name)}
+          {generateComponents(pack.name)}
+          {generateConstants(pack.name)}
+          {generateTypes(pack.name)}
+          {generateMethods(pack.name)}
       </div>
     ));
-    const showList = () => props.packages.map((pack) => (
-      <ul class="pack-list">
-          <div class="pack-list__name">{pack.moduleName}</div>
-        <ul class="pack-list__entities">{showListItem(pack.moduleEntities)}</ul>
-      </ul>
-    ));
     return () => (
-      showList()
+      generateList()
     );
   }
 };
